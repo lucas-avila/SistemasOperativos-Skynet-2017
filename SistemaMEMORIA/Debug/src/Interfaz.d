eea0e057220e8e700de6113607f0ad4713718ae8
Lucas Polonuer <polonuer@gmail.com>
2017-04-23 13:30:58 -0300

    Agregadas carpeta testing y general en kernel

--- SistemaMEMORIA/Debug/src/Interfaz.d
+++ SistemaMEMORIA/Debug/src/Interfaz.d
@@ -1,6 +1,8 @@
 src/Interfaz.d: ../src/Interfaz.c ../src/header/Interfaz.h \
- ../src/general/Socket.h
+ ../src/general/Socket.h ../src/header/MemoriaPrincipal.h
 
 ../src/header/Interfaz.h:
 
 ../src/general/Socket.h:
+
+../src/header/MemoriaPrincipal.h:
