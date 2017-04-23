96ef39ccb73e4e706373ed50042ebfdd6ef1307e
Jonathan <jvictoricacrescente@est.frba.utn.edu.ar>
2017-04-23 20:09:01 -0300

    Add files via upload

---
+++ SistemaKERNEL/src/testing/TestingInterfazMemoria.h
@@ -0,0 +1,13 @@
+/*
+ * TestingInterfazMemoria.h
+ *
+ *  Created on: 20/4/2017
+ *      Author: utnso
+ */
+
+#ifndef TESTING_TESTINGINTERFAZMEMORIA_H_
+#define TESTING_TESTINGINTERFAZMEMORIA_H_
+void testear_intefaz();
+
+
+#endif /* TESTING_TESTINGINTERFAZMEMORIA_H_ */
