1a4fed39e93600b90bc77cc5f8c5443a149e5e64
Lucas Avila <lucasrotelavila@gmail.com>
2017-05-07 01:37:28 -0300

    Faltaba que se puedan conectar multiples consolas

--- SistemaConsola/src/header/InterfazKernel.h
+++ SistemaConsola/src/header/InterfazKernel.h
@@ -9,7 +9,8 @@
 #define HEADER_INTERFAZKERNEL_H_
 
 char * enviar_programa_ANSISOP(char * path_archivo_fuente, int kernel);
-void CU_handshake(int kernel);
+void CU_handshake_consola(int kernel);
+void CU_handshake_programa(int kernel);
 
 void enviar_solicitud_cancelar_proceso();
 
